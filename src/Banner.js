ec375233b809c790c87d1f70c91542b17c85bedb
Devansh Sharma <devanshsharma@Devanshs-MacBook-Air.local>
2022-10-03 01:09:14 +0530

    upto movies

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect,useState} from 'react'
 import "./Banner.css"
+import axios from "./axios";
+import requests from "./Requests"
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    },[])
+
+    console.log(movie);
 
     function truncate(string,n) {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
@@ -11,19 +30,21 @@ function Banner() {
       className="banner"
           style={{
           backgroundSize:"cover",
-              backgroundImage: `url('https://www.passionateinmarketing.com/wp-content/uploads/2022/08/netflix.jpg')`,
+              backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition:"center center",
       }}
       >
           <div className="banner__contents">
-              <h1 className="banner__title">Movie Name</h1>
+              <h1 className="banner__title">
+                  {movie?.title||movie?.original_name}
+              </h1>
               <div className="banner__buttons">
                   <button className='banner__button'>Play</button>
                   <button className='banner__button'>My List</button>
               </div>
               <h1 className="banner__description">
                   {truncate(
-                      `This is a tests description `,150
+                      movie?.overview,160
                   )}
                   
               </h1>
